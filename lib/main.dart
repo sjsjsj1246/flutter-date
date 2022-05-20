b0dcab12127710d502ac48afb26bba8caa36e161
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-05-20 18:11:33 +0900

    init project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:date/screen/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(
+          fontFamily: 'sunflower',
+          textTheme: const TextTheme(
+              headline1: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'parisienne',
+                  fontSize: 80.0),
+              headline2: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.w700),
+              bodyText1: TextStyle(color: Colors.white, fontSize: 30.0),
+              bodyText2: TextStyle(color: Colors.white, fontSize: 20.0))),
+      home: HomeScreen()));
+}
